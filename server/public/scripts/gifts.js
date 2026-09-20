b956b83d287e4942137e1021f625664da56cc4b8
satyamd8 <satyamdhar8@gmail.com>
2026-09-20 12:43:16 -0400

    added postgres database connection/support

--- server/public/scripts/gifts.js
+++ server/public/scripts/gifts.js
@@ -37,7 +37,6 @@ const renderGifts = async () => {
 
          card.appendChild(topContainer);
          card.appendChild(bottomContainer);
-
          mainContent.appendChild(card);
       });
    } else {
@@ -47,49 +46,10 @@ const renderGifts = async () => {
    }
 };
 
-const renderGift = async () => {
-   const requestedID = parseInt(window.location.href.split("/").pop());
+const requestedUrl = window.location.href.split("/").pop();
 
-   const response = await fetch("/gifts");
-   const data = await response.json();
-
-   const giftContent = document.getElementById("gift-content");
-
-   let gift;
-
-   if (data) {
-      gift = data.find((gift) => gift.id === requestedID);
-   }
-
-   if (gift) {
-      document.getElementById("image").src = gift.image;
-      document.getElementById("image").alt = gift.name;
-
-      document.getElementById("name").textContent = gift.name;
-      document.getElementById("submittedBy").textContent =
-         "Submitted by: " + gift.submittedBy;
-      document.getElementById("submittedOn").textContent =
-         "Submitted on: " + gift.submittedOn;
-      document.getElementById("pricePoint").textContent =
-         "Price: " + gift.pricePoint;
-      document.getElementById("audience").textContent =
-         "Great For: " + gift.audience;
-      document.getElementById("description").textContent = gift.description;
-
-      document.title = `UnEarthed - ${gift.name}`;
-   } else {
-      const message = document.createElement("h2");
-      message.textContent = "No Gifts Available 😞";
-      giftContent.appendChild(message);
-   }
-};
-
-const giftPath = window.location.pathname.match(/^\/gifts\/(\d+)\/?$/);
-
-if (giftPath) {
-   renderGift();
-} else if (window.location.pathname === '/') {
-   renderGifts();
+if (requestedUrl) {
+   window.location.href = "../404.html";
 } else {
-   window.location.href = '/404.html';
+   renderGifts();
 }
